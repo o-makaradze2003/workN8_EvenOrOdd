06d007c95e4d836f263a718ffc853025dc28eb95
o-makaradze2003 <omakaradze8@gmail.com>
2024-07-10 14:22:45 +0400

    after refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-   EvenOrOdd even=new EvenOrOdd();
-        even.EvenOrOdd();
+        System.out.println(makeLIstFromWord("adfzd"));
+    }
+
+    public static List<String> makeLIstFromWord(String word) {
+        if (word.length() % 2 != 0) {
+            word += "_";
+        }
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < word.length(); i += 2) {
+            list.add(word.substring(i, i + 2));
+        }
+        return list;
     }
 }
